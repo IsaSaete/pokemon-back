1ab687feecbb436f7774ed7112214362547828f8
Isabel Sáenz <isasaete@gmail.comn>
2025-04-15 16:46:39 +0200

    Add PokemonRouter

--- src/server/app.ts
+++ src/server/app.ts
@@ -2,12 +2,15 @@ import express from "express";
 import morgan from "morgan";
 import checkHealthStatus from "./middlewares/checkHealthStatus/checkHealthStatus";
 import handleEndpointNotFound from "./middlewares/handleEndpointNotFount/handleEndpointNotFound";
+import pokemonRouter from "../router/pokemonRouter";
 
 const app = express();
 
 app.use(morgan("dev"));
 
 app.get("/", checkHealthStatus);
+
+app.use("/pokemon", pokemonRouter);
 
 app.use(handleEndpointNotFound);
 
