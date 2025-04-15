50efa158a3645d33d544b80b424458ab60707713
Isabel Sáenz <isasaete@gmail.comn>
2025-04-15 14:25:45 +0200

    Add test PokemonController & add fixtures

---
+++ src/controller/getPokemons.test.ts
@@ -0,0 +1,35 @@
+import { Request, Response } from "express";
+import { testPokemons } from "../pokemon/fixtures";
+import PokemonController from "./PokemonController";
+
+describe("Gicen a getPokemons function", () => {
+  describe("When it receives a response", () => {
+    const pokemonController = new PokemonController(testPokemons);
+
+    const req = {} as Request;
+    const res = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+    } as Pick<Response, "status" | "json">;
+
+    beforeEach(() => {
+      jest.clearAllMocks();
+    });
+
+    test("Then it should call the received response's method status with 200", () => {
+      const expectedStatus = 200;
+
+      pokemonController.getPokemons(req as Request, res as Response);
+
+      expect(res.status).toHaveBeenCalledWith(expectedStatus);
+    });
+
+    test("Then it should call the received response's method json with 'Buterfree', 'Zubat' and 'Paras' pokemons", () => {
+      const expectedPokemons = { pokemons: testPokemons };
+
+      pokemonController.getPokemons(req as Request, res as Response);
+
+      expect(res.json).toHaveBeenCalledWith(expectedPokemons);
+    });
+  });
+});
