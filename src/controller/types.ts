2288b1698113688f8090cfc90e4f5f2472cd9da2
Isabel Sáenz <isasaete@gmail.comn>
2025-04-17 21:36:43 +0200

    Add method addPokemon to interface PokemonControllerStructure

--- src/controller/types.ts
+++ src/controller/types.ts
@@ -2,4 +2,5 @@ import { Request, Response } from "express";
 
 export interface PokemonControllerStructure {
   getPokemons: (req: Request, res: Response) => void;
+  addPokemon: (req: Request, res: Response) => void;
 }
